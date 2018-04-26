314a098e684e6de387a8926adf397336284af538
Totofish <eanean120@gmail.com>
2018-04-27 01:51:17 +0800

    feat: 欄位驗證提示紅點

--- src/extension/mock-api.js
+++ src/extension/mock-api.js
@@ -1,10 +1,51 @@
 import './mock-api.scss';
 
+let inputs = {};
+let submitted = false;
+
+function classControl(element, bo) {
+  if (bo) element.classList.remove("label--warning");
+  else element.classList.add("label--warning");
+}
+
+// 欄位驗證
+function verify() {
+  const mockURL = inputs.mockURL.value.trim();
+  const status = inputs.status.value.trim();
+  const response = inputs.response.value.trim();
+  const timeout = inputs.timeout.value.trim() || '100-200';
+
+  const verificationURL = !!mockURL;
+  const verificationStatus = !!status;
+  let verificationResponse = !!response;
+
+
+  try {
+    JSON.parse(response);
+  } catch(e) {
+    verificationResponse = false;
+  }
+
+  classControl(document.querySelector('.label-url'), verificationURL);
+  classControl(document.querySelector('.label-status'), verificationStatus);
+  classControl(document.querySelector('.label-response'), verificationResponse);
+
+  return {
+    response,
+    mockURL,
+    timeout,
+    status
+  }
+}
+
 function mock(e) {
-  const response = document.getElementById('response').value;
-  const mockURL = document.getElementById('mockURL').value;
-  const timeout = document.getElementById('timeout').value || '100-200';
-  const status = document.getElementById('status').value;
+  submitted = true;
+  const {
+    response,
+    mockURL,
+    timeout,
+    status
+  } = verify();
   if (!mockURL || !response || !status) {
     new Notification("請確認欄位是否設定完成", { icon: '../icon48.png' });
     return;
@@ -31,7 +72,21 @@ function mock(e) {
   window.close();
 }
 
+// 欄位值改變
+function handleChange(e) {
+  if (!submitted) return;
+  verify();
+}
+
+
 document.addEventListener('DOMContentLoaded', function () {
+  inputs = {
+    mockURL: document.querySelector('#mockURL'),
+    status: document.querySelector('#status'),
+    response: document.querySelector('#response'),
+    timeout: document.querySelector('#timeout'),
+  };
+  // 預設值
   let mockExtensionData = {
     response: '',
     mockURL: '',
@@ -44,15 +99,22 @@ document.addEventListener('DOMContentLoaded', function () {
     localStorage.removeItem('mockExtensionData');
   }
   try {
-    document.getElementById('response').value = JSON.stringify(JSON.parse(mockExtensionData.response), null, 2);
+    inputs.response.value = JSON.stringify(JSON.parse(mockExtensionData.response), null, 2);
   } catch (e) {
-    document.getElementById('response').value = mockExtensionData.response;
+    inputs.response.value = mockExtensionData.response;
   }
-  document.getElementById('mockURL').value = mockExtensionData.mockURL;
-  document.getElementById('timeout').value = mockExtensionData.timeout;
-  document.getElementById('status').value = mockExtensionData.status;
+  inputs.mockURL.value = mockExtensionData.mockURL;
+  inputs.timeout.value = mockExtensionData.timeout;
+  inputs.status.value = mockExtensionData.status;
+
   const divs = document.querySelectorAll('#btn');
   for (var i = 0; i < divs.length; i++) {
     divs[i].addEventListener('click', mock);
   }
+
+  // 偵測 input 變化
+  Object.keys(inputs).forEach((key) => {
+    inputs[key].addEventListener('change', handleChange);
+    inputs[key].addEventListener('keyup', handleChange);
+  });
 });
